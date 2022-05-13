36a7657d137fd49551cedd1067655fb2f9c9f01c
karthik202000 <92912370+karthik202000@users.noreply.github.com>
2022-05-13 11:45:31 +0530

    Add files via upload

---
+++ mobile.java
@@ -0,0 +1,16 @@
+package week1.day1;
+
+public class mobile {
+	public static void main(String[] args) {
+		String mobileBrand="Realme";
+		System.out.println("Name of the brand:" +mobileBrand);
+		char androidVersion='9';
+		System.out.println("Name of the version:" +androidVersion);
+		int price=20000;
+		System.out.println("Price of the mobile:" +price);
+		String ram="8GB";
+		System.out.println("Ram:" +ram);
+		String rom="256GB";
+		System.out.println("Rom:" +rom);
+	}
+}
